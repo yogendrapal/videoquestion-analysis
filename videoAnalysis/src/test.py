79d57fbe22544861629abcc840e62e991dcd18c1
Neel Dani <neeldani98@gmail.com>
2019-05-29 17:40:20 +0530

    Used SSD for face detection and stored tags in JSON

--- videoAnalysis/src/test.py
+++ videoAnalysis/src/test.py
@@ -1,11 +1,59 @@
+'''
+
+Github link of original code: https://github.com/oarriaga/face_classification/blob/master/report.pdf
+
+Author: Neel Dani
+Date: 22nd May, 2019
+
+Incroporated few changes according to needs mentioned below.
+
+CHANGES INCORPORATED:
+
+1) Used skvideo.io package to read a video rather than openCV as video maybe rotated (landscape mode). 
+Skvideo.io also reads metdata about the video, a provision missing in OpenCV.
+
+2) Used cvlib (SSD model of OpenCV) for face detection which performs better than the Haar Cascade. 
+Haar Cascade detects only frontal faces.
+
+3) Read video as a JSON object and write the ouput (tags of gender, emotion etc) as a JSON file
+so that it is easily accessible from cloud.
+
+4) P(female) > 0.4 and not 0.5. This change is incorporated as the CNN gender model is trained on faces 
+of Western people. As a result, it has a bias towards Westerneres and doesnt work perfectly on Indian images.
+The model thus makes mistakes by predicting female as male. Hence to remove this bias, P(female) > 0.4 instead of 0.5.
+  
+5)Evaluation metric is calculated by taking majority of the last "n" (frame_wndow) frames.   
+
+'''
+
+
+'''
+
+TO DO
+
+1) Face alignment algorithm after face detection for better prediction.
+
+2) Merge age prediction code with gender and expression prediction.
+
+
+JSON should have following fields:
+1) Filename
+2) Timestamp pertaining to co-ordiantes of face
+3) Face-co-ordinates, Confidence fora particular time-stamp(? Ask sir)
+4) Gender tag
+
+'''
+
 from statistics import mode
 
 import cv2
 from keras.models import load_model
 import numpy as np
 
+#added imports to support changes
 import skvideo.io
 from utils.inference import apply_offset
+import cvlib as cv
 import json
 
 from utils.datasets import get_labels
@@ -16,21 +64,30 @@ from utils.inference import apply_offsets
 from utils.inference import load_detection_model
 from utils.preprocessor import preprocess_input
 
-'''
-global variables declared by me
-'''
-male = 0
-female = 0
+# Count of the genders predicted for each frame. Final prediction will be max(male_count, female_count)
+gender_count = {"man": 0, "woman": 0}
 
 # parameters for loading data and images
 detection_model_path = '../trained_models/detection_models/haarcascade_frontalface_default.xml'
 emotion_model_path = '../trained_models/emotion_models/fer2013_mini_XCEPTION.102-0.66.hdf5'
 gender_model_path = '../trained_models/gender_models/simple_CNN.81-0.96.hdf5'
 
-'''
-path to video
-'''
+#path to video
 path = "/home/neel/IITB/face_classification/images/dimpal.mp4"
+
+#JSON file paths
+json_files_path = "../json_files/"
+json_video_load_path = json_files_path + "video_path.json"
+
+with open(json_video_load_path, 'r') as infile:
+    video_path = json.loads(infile.read())
+video_name = video_path["path"].split('/')[-1].split('.')[0]
+json_tag_write_path = json_files_path + video_name + ".json"
+
+print(json_video_load_path)
+print(video_path)
+print(video_name)
+print(json_tag_write_path)
 
 emotion_labels = get_labels('fer2013')
 gender_labels = get_labels('imdb')
@@ -38,8 +95,15 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 
 # hyper-parameters for bounding boxes shape
 frame_window = 10
-gender_offsets = (30, 60) # Buffer for bounding boxes for gender
-emotion_offsets = (20, 40) # Buffer for bounding boxes for emotion
+
+# Buffer for bounding boxes for gender
+gender_offsets = (30, 60) 
+
+# Buffer for bounding boxes for emotion
+emotion_offsets = (20, 40) 
+
+#No. of faces required
+num_faces = 1
 
 # loading models
 face_detection = load_detection_model(detection_model_path)
@@ -59,19 +123,31 @@ emotion_window = []
 
 # starting video streaming
 cv2.namedWindow('window_frame')
-videogen = skvideo.io.vreader(path)
+videogen = skvideo.io.vreader(video_path["path"])
 
 for bgr_image in videogen:
 
     bgr_image = cv2.cvtColor(bgr_image, cv2.COLOR_RGB2BGR)
     frame_dim = bgr_image.shape[:2]
-    #bgr_image = video_capture.read()[1]
+    
     gray_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
     rgb_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2RGB)
-    faces = detect_faces(face_detection, gray_image) #detect multi-scale images using Haar Cascade
 
-    for face_coordinates in faces:
+    #Detect multiple face coooridnates along with their confidences using SSD from cvlib library
+    faces, confidences = cv.detect_face(bgr_image)
 
+    for detected_faces_index in range(min(len(faces), num_faces)):
+        
+        '''
+        extract co-ordinates of the detected face.
+        (startX, startY) is the bottom-left co-orinate of the detected face.
+        (endX, endY) is the top-right co-ordinate of the detected face.
+        '''
+        face_coordinates = faces[detected_faces_index]
+        startX, startY, endX, endY = face_coordinates
+
+        #Transform co-ordinates from (x1, y1, x2, y2) to (x1, y1, width, height)
+        face_coordinates = [startX, startY, endX-startX, endY-startY]
         x1, x2, y1, y2 = apply_offset(face_coordinates, gender_offsets, frame_dim) #for expanding the bounding box
         rgb_face = rgb_image[y1:y2, x1:x2]
         gray_face = gray_image[y1:y2, x1:x2]
@@ -83,15 +159,19 @@ for bgr_image in videogen:
         except:
             continue
 
+
         gray_face = preprocess_input(gray_face, False) #if true, it does normalization
+        
+        '''
+        the below 2 lines involving np.expand_dims makes the 
+        dim of the array from (x, y) to (1, x, y, 1). The first dim denotes the
+        batch size and the last one denotes the no. of channels; grayscale in this case
+        and hence 1. (3 in case of RBG).
+        '''
+
         gray_face = np.expand_dims(gray_face, 0)
         gray_face = np.expand_dims(gray_face, -1)
 
-        '''
-        the above 2 steps np.expand_dims makes the 
-        dim of the array from (x, y) to (1, x, y, 1). The first one denotes the
-        batch size and the last one denotes the channels; grayscale in this case
-        '''
 
         emotion_prob = emotion_classifier.predict(gray_face)
         emotion_label_arg = np.argmax(emotion_prob)
@@ -120,15 +200,11 @@ for bgr_image in videogen:
 
         if gender_text == gender_labels[0]:
             color = (0, 0, 255)
-            gender_text += str(gender_prediction[0])
+            gender_text += " " + str(gender_prediction[0])
 
         else:
             color = (255, 0, 0)
             gender_text += " " + str(gender_prediction[1])
-           
-        #female += gender_prediction[0]
-        #male += 1 - gender_prediction[0]
-        print(gender_text)
         
         draw_bounding_box(face_coordinates, rgb_image, color)
         draw_text(face_coordinates, rgb_image, gender_text + str(gender_prediction[0]),
@@ -136,12 +212,20 @@ for bgr_image in videogen:
         draw_text(face_coordinates, rgb_image, emotion_mode,
                   color, 0, -45, 1, 1)
 
+        #increment man or woman count based on the tag
+        gender_count[gender_mode] += 1
+
     bgr_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2BGR)
     cv2.imshow('window_frame', bgr_image)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-#print("Male Expected count: ", male)
-#print("Female Expected count: ", female)
-#print("P(Male): ", male/(male + female))
-#print("P(Female): ", female/(male + female))
+print("Man count: ", gender_count["man"])
+print("Woman count: ", gender_count["woman"])
+predicted_gender = max(gender_count, key=gender_count.get)
+print("Predicted gender: ",  predicted_gender)
+
+tags = {"Gender": predicted_gender}
+
+with open(json_tag_write_path, 'w') as outfile:
+    json.dump(tags, outfile)
