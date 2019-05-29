293f9e656f35540369d491f19d4a34e5ce792411
Neel Dani <neeldani98@gmail.com>
2019-05-29 10:08:00 +0530

    Initial commit

---
+++ videoAnalysis/src/video_emotion_gender_demo.py
@@ -0,0 +1,115 @@
+from statistics import mode
+
+import cv2
+from keras.models import load_model
+import numpy as np
+
+from utils.datasets import get_labels
+from utils.inference import detect_faces
+from utils.inference import draw_text
+from utils.inference import draw_bounding_box
+from utils.inference import apply_offsets
+from utils.inference import load_detection_model
+from utils.preprocessor import preprocess_input
+
+# parameters for loading data and images
+detection_model_path = '../trained_models/detection_models/haarcascade_frontalface_default.xml'
+emotion_model_path = '../trained_models/emotion_models/fer2013_mini_XCEPTION.102-0.66.hdf5'
+gender_model_path = '../trained_models/gender_models/simple_CNN.81-0.96.hdf5'
+emotion_labels = get_labels('fer2013')
+gender_labels = get_labels('imdb')
+font = cv2.FONT_HERSHEY_SIMPLEX
+
+# hyper-parameters for bounding boxes shape
+frame_window = 10
+gender_offsets = (30, 60) #Buffer for bounding boxes for gender
+emotion_offsets = (20, 40) #Buffer for bounding boxes for emotion
+
+# loading models
+face_detection = load_detection_model(detection_model_path)
+emotion_classifier = load_model(emotion_model_path, compile=False)
+gender_classifier = load_model(gender_model_path, compile=False)
+
+# getting input model shapes for inference
+emotion_target_size = emotion_classifier.input_shape[1:3] #requires input shape of (64, 64) for fer2013_mini_XCEPTION.102-0.66.hdf5
+gender_target_size = gender_classifier.input_shape[1:3] #requires input shape of (48, 48) simple_CNN.81-0.96.hdf5'
+
+print("Emotion target size: ", emotion_target_size)
+print("Gender target size: ", gender_target_size)
+
+# starting lists for calculating modes
+gender_window = []
+emotion_window = []
+
+# starting video streaming
+cv2.namedWindow('window_frame')
+video_capture = cv2.VideoCapture(0)
+
+while True:
+
+    bgr_image = video_capture.read()[1]
+    gray_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
+    rgb_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2RGB)
+    faces = detect_faces(face_detection, gray_image) #detect multi-scale images using Haar Cascade
+
+    for face_coordinates in faces:
+
+        print(face_coordinates)
+        x1, x2, y1, y2 = apply_offsets(face_coordinates, gender_offsets) #for expanding the bounding box
+        rgb_face = rgb_image[y1:y2, x1:x2]
+
+        x1, x2, y1, y2 = apply_offsets(face_coordinates, emotion_offsets) #for expanding bounding box
+        gray_face = gray_image[y1:y2, x1:x2]
+        try:
+            rgb_face = cv2.resize(rgb_face, (gender_target_size))
+            gray_face = cv2.resize(gray_face, (emotion_target_size))
+        except:
+            continue
+
+        gray_face = preprocess_input(gray_face, False) #If true, it does normalization
+        gray_face = np.expand_dims(gray_face, 0)
+        gray_face = np.expand_dims(gray_face, -1)
+
+        '''
+        the above 2 steps np.expand_dims makes the 
+        dim of the array from (x, y) to (1, x, y, 1). The first one denotes the
+        batch size and the last one denotes the channels; grayscale in this case
+        '''
+
+        emotion_label_arg = np.argmax(emotion_classifier.predict(gray_face))
+        emotion_text = emotion_labels[emotion_label_arg]
+        emotion_window.append(emotion_text)
+
+        rgb_face = np.expand_dims(rgb_face, 0)
+        rgb_face = preprocess_input(rgb_face, False)
+        gender_prediction = gender_classifier.predict(rgb_face)
+        gender_label_arg = np.argmax(gender_prediction)
+        gender_text = gender_labels[gender_label_arg]
+        gender_window.append(gender_text)
+
+        if len(gender_window) > frame_window:
+            emotion_window.pop(0)
+            gender_window.pop(0)
+        try:
+            emotion_mode = mode(emotion_window)
+            gender_mode = mode(gender_window)
+        except:
+            continue
+
+        if gender_text == gender_labels[0]:
+            color = (0, 0, 255)
+        else:
+            color = (255, 0, 0)
+
+        draw_bounding_box(face_coordinates, rgb_image, color)
+        draw_text(face_coordinates, rgb_image, gender_mode,
+                  color, 0, -20, 1, 1)
+        draw_text(face_coordinates, rgb_image, emotion_mode,
+                  color, 0, -45, 1, 1)
+
+    bgr_image = cv2.cvtColor(rgb_image, cv2.COLOR_RGB2BGR)
+    cv2.imshow('window_frame', bgr_image)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+video_capture.release()
+cv2.destroyAllWindows()
